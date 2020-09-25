7abf2b943a1f641f4713c7604f9aa111c53d5c33
Aimie Ojuba <41306649+Cytla24@users.noreply.github.com>
2020-09-24 23:31:15 -0400

    Create signal.c

---
+++ signal.c
@@ -0,0 +1,27 @@
+/* hello_signal.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+int glob = 0;
+
+void handler(int signum)
+{ //signal handler
+  printf("Hello World!\n");
+  glob = 1;
+}
+
+int main(int argc, char * argv[])
+{
+  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  while(1){
+    alarm(1); //Schedule a SIGALRM for 1 second
+    while(glob==0); //busy wait for signal to be delivered
+    printf("Turing was right!\n");
+    glob = 0;
+  }
+  
+  
+  return 0; //never reached
+}
